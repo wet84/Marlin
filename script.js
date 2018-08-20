70b78d8e86a4d918fab2b2256b118235d9d4eaf4
Wet <n.user.it@gmail.com>
2018-08-20 15:39:01 +0300

    test version2

---
+++ script.js
@@ -0,0 +1,48 @@
+$(window).ready(function(){
+    $('.big-caption, .logo').css('opacity', '1');
+});
+
+AOS.init();
+
+jQuery(document).ready(function($){
+    $('.enter').on('click', function(){
+        $('.overlay').animate({
+              opacity: "1",
+              opacity: "0",
+              display: "none"
+            },
+          2000);
+        $('.custom-modal').removeClass('error-overlay');
+        clearInterval(stopAnimate);
+    });
+    
+    $('.exit').on('click', function(){
+        $('.custom-modal').addClass('error-overlay');
+    });
+
+
+    var bArray = [];
+    var sArray = [4,6,8,10,15,20];
+ 
+    for (var i = 0; i < $('.bubbles').width(); i++) {
+        bArray.push(i);
+    }
+     
+    function randomValue(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+ 
+    var stopAnimate = setInterval(function(){
+         
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity' : '-=0.5'
+        }, 5000, function(){
+            $(this).remove()
+        }
+        );
+    }, 350);
+});
+
